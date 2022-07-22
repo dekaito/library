683d64d7e6697e1e1347ac58884a124d5b451528
dekaito <105399498+dekaito@users.noreply.github.com>
2022-07-22 13:04:45 +0800

    Update aroxUI.lua

--- aroxUI.lua
+++ aroxUI.lua
@@ -29,7 +29,7 @@ local Library = {
 
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(0, 13, 75);
+    BackgroundColor = Color3.fromRGB(204, 255, 55);
     AccentColor = Color3.fromRGB(255,182,193);
     OutlineColor = Color3.fromRGB(50, 50, 50);
 
